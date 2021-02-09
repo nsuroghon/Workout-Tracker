0fd26139b3083bbc5c10c17c7cf11a3b930b00d5
Niv Suroghon <nsuroghon@gmail.com>
2021-02-09 15:31:20 -0500

    removed required in model structure

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -13,57 +13,36 @@ const workoutSchema = new Schema({
         type: {
             type: String,
             trim: true,
-            required: "exercise type is required",
         },
         name: {
             type: String,
             trim: true,
-            required: "name is required",
+            
         },
         duration: {
             type: Number,
             trim: true,
-            required: "duration is required",
+            
         },
         weight: {
             type: Number,
             trim: true,
-            required: "weight is required",
+        
         },
         reps: {
             type: Number,
             trim: true,
-            required: "reps is required",
+        
         },
         sets: {
             type: Number,
             trim: true,
-            required: "sets is required",
+        
         },
       }
     ]
 })
 
-// username: {
-//     type: String,
-//     trim: true,
-//     required: "Username is Required"
-//   },
-
-
-// {
-//     day: new Date().setDate(new Date().getDate()-10),
-//     exercises: [
-//       {
-//         type: "resistance",
-//         name: "Bicep Curl",
-//         duration: 20,
-//         weight: 100,
-//         reps: 10,
-//         sets: 4
-//       }
-//     ]
-//   },
   const Workout = mongoose.model("Workout", workoutSchema);
   
   module.exports = Workout;
